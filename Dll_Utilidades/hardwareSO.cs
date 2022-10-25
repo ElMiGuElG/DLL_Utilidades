ec609a261714311cc9aa83380d72d386d10f8c84
Miguel Galeano <miguel.galeano987@pascualbravo.edu.co>
2022-10-24 19:55:37 -0500

    Agregar archivos de proyecto.

---
+++ Dll_Utilidades/hardwareSO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+/// <summary>
+/// 
+/// </summary>
+/// 
+
+namespace Dll_Utilidades
+{
+    public class hardwareSO
+    {
+
+    }
+}
